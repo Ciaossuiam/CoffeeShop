c750b48b5cdab45e9d1cb1ae81691c70aa59efc8
mouthleechewei@gmail.com <mouthleechewei@gmail.com>
2023-08-26 13:14:07 +0800

    加入必要元素 ( 品項名稱、價格、大小、備註 )

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import OrderList from './components/OrderList.vue';
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+    <OrderList />
 </template>
 
 <style scoped>
---
+++ src/components/OrderList.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- <h1>{{ msg }}</h1> -->
+
+  <div>
+    <!-- <button type=button" @click="count++">count is {{ count }}</button> -->
+    <h2 class="order">訂單</h2>
+    <div>
+        <label for="name">品項名稱：</label>
+        <input type="text" id="name">
+    </div>
+    <div>
+        <label for="price">價格：</label>
+        <input type="text" id="price">
+    </div>
+    <div>
+        <label for="size">大小：</label>
+        <select id="size">
+            <option value="L">L</option>
+            <option value="M">M</option>
+            <option value="S">S</option>
+        </select>
+    </div>
+    <div>
+        <label for="note">備註：</label>
+        <input type="text" id="note">
+    </div>
+    <button type="button">新增</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
